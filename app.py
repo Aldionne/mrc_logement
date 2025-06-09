22036fcf9a94bb4ae81ffce4b3822547da3dd5cf
Aldionne <aldionne@uqac.ca>
2025-06-09 16:19:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def load_data():
 data = load_data()
 
 # Titre
-st.title("Indicateurs de l'état du logement et l'habitation par MRC")
+st.title("Portrait de l'habitation par MRC")
 
 # Sélection de la MRC
 selected_mrc = st.selectbox("Choisissez une MRC", sorted(data["MRC"].unique()))
@@ -32,17 +32,18 @@ if not mrc_data.empty:
         "Nombre de logements": [int(mrc_row[year]) for year in years]
     })
 
-    st.subheader(f"Évolution du nombre de logements (2015-2025): {selected_mrc}")
+    st.subheader(f"Évolution du nombre de logements (2015-2025) {selected_mrc}")
 
     # Création du graphique avec Altair
-    chart = alt.Chart(logement_data).mark_line(point=True).encode(
-        x=alt.X("Année:O", title="Année"),
-        y=alt.Y("Nombre de logements:Q", title="Nombre de logements"),
-        tooltip=["Année", "Nombre de logements"]
-    ).interactive().properties(
-        width=700,
-        height=400
-    )
+   chart = alt.Chart(logement_data).mark_line(point=alt.OverlayMarkDef(filled=True, size=100), strokeWidth=3).encode(
+    x=alt.X("Année:O", title="Année"),
+    y=alt.Y("Nombre de logements:Q", title="Nombre de logements"),
+    tooltip=["Année", "Nombre de logements"]
+).properties(
+    width=700,
+    height=400,
+    title=f"Évolution du nombre de logements - {selected_mrc}"
+).interactive()
 
     st.altair_chart(chart, use_container_width=True)
 
